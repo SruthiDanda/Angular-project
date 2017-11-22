f3cfccdcd261dc111470a6189bbba9f25731d40f
sruthidanda <sruthidanda5@gmail.com>
2017-11-22 05:58:08 -0800

    added files to project

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,10 +9,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
-  constructor() { }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  // heroes = HEROES;
+  heroes: Hero[];
+   selectedHero: Hero;
+  
+   constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
+    ngOnInit() {
+      this.getHeroes();
+    }
+  
+ 
+   
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
